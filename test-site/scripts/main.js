aa1109c5620f0ecc2d02175c149ff85a29748a66
maximkapotapov <38438358+maximkapotapov@users.noreply.github.com>
2019-12-21 18:58:58 +0300

    Add files via upload

---
+++ test-site/scripts/main.js
@@ -0,0 +1,35 @@
+document.querySelector('h1').onclick = function() {
+	alert('Не тыкай в меня! :(');
+}
+
+var myImage = document.querySelector('img');
+myImage.onclick = function() {
+	var mySrc = myImage.getAttribute('src');
+	if (mySrc === 'images/1.jpg') {
+		myImage.setAttribute ('src', 'images/2.jpg');
+	} 
+    else {
+    	myImage.setAttribute ('src', 'images/1.jpg');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+	var myName = prompt ('Введите имя, пожалуйста')
+	localStorage.setItem('name', myName);
+	myHeading.textContent = 'Привет, ' + myName;
+}
+
+if (!localStorage.getItem('name')) {
+	setUserName();
+}
+else {
+	var storedName = localStorage.getItem('name');
+	myHeading.textContent = 'Привет, ' + storedName;
+}
+
+myButton.onclick = function() {
+	setUserName();
+}
